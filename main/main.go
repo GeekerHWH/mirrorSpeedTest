73296effcc55c9c7e0a7afad8d27bbb15b3c955a
GeekerHWH <hacker.huwenhao@gmail.com>
2024-01-31 09:00:59 +0800

    support multi choose mirrors to test

--- main/main.go
+++ main/main.go
@@ -6,6 +6,7 @@ import (
 	"mirrorsTestTools/main/cfginput"
 	"mirrorsTestTools/main/tester"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -43,36 +44,84 @@ func main() {
 			mirrorURLs = append(mirrorURLs, input)
 		}
 
-	case 2:
-		// coming soon...
-		// mirrorURL =
-		return
-
-	case 3:
-		return
-	}
-
-	// 执行测试
-	if len(mirrorURLs) == 4 {
-		downloadSpeed, err := tester.TestMirrorSpeed(mirrorURLs[1])
-
-		// 处理测试结果
-		if err != nil {
-			fmt.Printf("测试失败：%s\n", err)
-		} else {
-			fmt.Printf("从 %s 下载 Debian Release 文件的速度：%f MB/s\n", mirrorNames[1], downloadSpeed)
-		}
-	} else {
-		for i := len(mirrorNames); i < len(mirrorURLs); i++ {
-			downloadSpeed, err := tester.TestMirrorSpeed(mirrorURLs[i])
+		// 执行测试
+		if len(mirrorURLs) == 4 {
+			downloadSpeed, err := tester.TestMirrorSpeed(mirrorURLs[1])
 
 			// 处理测试结果
 			if err != nil {
 				fmt.Printf("测试失败：%s\n", err)
 			} else {
-				fmt.Printf("从 %s 下载 Debian Release 文件的速度：%f MB/s\n", mirrorURLs[i], downloadSpeed)
+				fmt.Printf("从 %s 下载 Debian Release 文件的速度：%f MB/s\n", mirrorNames[1], downloadSpeed)
+			}
+		} else {
+			for i := len(mirrorNames); i < len(mirrorURLs); i++ {
+				downloadSpeed, err := tester.TestMirrorSpeed(mirrorURLs[i])
+
+				// 处理测试结果
+				if err != nil {
+					fmt.Printf("测试失败：%s\n", err)
+				} else {
+					fmt.Printf("从 %s 下载 Debian Release 文件的速度：%f MB/s\n", mirrorURLs[i], downloadSpeed)
+				}
 			}
 		}
+
+	case 2:
+		fmt.Println("请选择要测试的镜像站(中间用空格隔开)：")
+		for i, name := range mirrorNames {
+			fmt.Printf("%d. %s\n", i+1, name)
+		}
+
+		// 读取用户选择的镜像站序号
+		var index string
+		reader := bufio.NewReader(os.Stdin)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("读取输入错误:", err)
+			return
+		}
+
+		// 移除换行符
+		index = strings.TrimSuffix(line, "\n")
+
+		// 以空格分割输入字符串
+		data := strings.Fields(index) //data: [1 3 4]
+
+		for _, d := range data {
+			// 将字符串转换为整数
+			i, err := strconv.Atoi(d)
+			if err != nil {
+				fmt.Println("无效的输入，请重新输入")
+			}
+			mirrorURLs = append(mirrorURLs, mirrorURLs[i-1])
+		}
+
+		// 执行测试
+		if len(mirrorURLs) == 4 {
+			downloadSpeed, err := tester.TestMirrorSpeed(mirrorURLs[1])
+
+			// 处理测试结果
+			if err != nil {
+				fmt.Printf("测试失败：%s\n", err)
+			} else {
+				fmt.Printf("从 %s 下载 Debian Release 文件的速度：%f MB/s\n", mirrorNames[1], downloadSpeed)
+			}
+		} else {
+			for i := len(mirrorNames); i < len(mirrorURLs); i++ {
+				downloadSpeed, err := tester.TestMirrorSpeed(mirrorURLs[i])
+
+				// 处理测试结果
+				if err != nil {
+					fmt.Printf("测试失败：%s\n", err)
+				} else {
+					fmt.Printf("从 %s 下载 Debian Release 文件的速度：%f MB/s\n", mirrorURLs[i], downloadSpeed)
+				}
+			}
+		}
+
+	case 3:
+		return
 	}
 
 }
